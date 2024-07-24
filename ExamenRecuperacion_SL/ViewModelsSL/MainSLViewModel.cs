2d09680984a9397c8f2955d099ec732168203fd7
mango2309 <sebastian.largo@udla.edu.ec>
2024-07-24 11:06:06 -0500

    Se modificó la lógica del View Models y se cambia la vista del menu

---
+++ ExamenRecuperacion_SL/ViewModelsSL/MainSLViewModel.cs
@@ -0,0 +1,47 @@
+﻿using ExamenRecuperacion_SL.ModelsSL;
+using ExamenRecuperacion_SL.ServicesSL;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenRecuperacion_SL.ViewModelsSL
+{
+    public class MainSLViewModel : INotifyPropertyChanged
+    {
+        private readonly RazaApiService _dogApiService;
+        private ObservableCollection<RazaPerroSL> _breeds;
+
+        public ObservableCollection<RazaPerroSL> Breeds
+        {
+            get => _breeds;
+            set
+            {
+                _breeds = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainSLViewModel()
+        {
+            _dogApiService = new RazaApiService();
+            LoadBreeds();
+        }
+
+        private async void LoadBreeds()
+        {
+            var breeds = await _dogApiService.GetBreedsAsync();
+            Breeds = new ObservableCollection<RazaPerroSL>(breeds);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ ExamenRecuperacion_SL/ViewsSL/MenuMainPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace ExamenRecuperacion_SL.ViewsSL;
+
+public partial class MenuMainPage : ContentPage
+{
+	public MenuMainPage()
+	{
+		InitializeComponent();
+	}
+}
